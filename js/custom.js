dc2f036521dd1ac12d59ae3e9165131522356400
s0369852 <s0369852@gmail.com>
2020-10-11 20:25:57 +0900

    swiper.js 사용하기

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+var mySwiper = new Swiper('.swiper-container', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }, 
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+    }, 
+    autoplay: {
+        delay: 5000,
+    },
+});
