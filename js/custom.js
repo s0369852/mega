b77f7434b954640ec1c5667686fbe66532571315
s0369852 <s0369852@gmail.com>
2020-10-14 22:53:34 +0900

    메가박스 youube API

--- js/custom.js
+++ js/custom.js
@@ -1,118 +1,121 @@
 'use strict';
+(function($){
 
-// 배너 이미지 슬라이드
-var mySwiper = new Swiper('.swiper-container', {
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    }, 
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-    }, 
-    autoplay: {
-        delay: 5000,
-    },
-});
+    // 배너 이미지 슬라이드
+    var mySwiper = new Swiper('.swiper-container', {
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        }, 
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+        }, 
+        autoplay: {
+            delay: 5000,
+        },
+    });
 
-// 영화차트 이미지 슬라이드
-var swiper = new Swiper('.swiper-container2', {
-    // 슬라이드가 제각기 따로 움직이고 싶다면 이름을 달리해주면 된다. 
-    slidesPerView: 4,
-    spaceBetween: 24,
-    mousewheel: {
-        invert: true,
-    },
-    // keyboard: {
-    //     enabled: true,
-    //     onlyInViewport: false,
-    // },
-    autoplay: {
-        delay: 3000,
-    },
-    breakpoints: {
-        // when window width is >= 600px
-        600: {
-            slidesPerView: 1.4,
-            spaceBetween: 24
+    // 영화차트 이미지 슬라이드
+    var swiper = new Swiper('.swiper-container2', {
+        // 슬라이드가 제각기 따로 움직이고 싶다면 이름을 달리해주면 된다. 
+        slidesPerView: 4,
+        spaceBetween: 24,
+        mousewheel: {
+            invert: true,
         },
-        // when window width is >= 768px
-        768: {
-            slidesPerView: 2,
-            spaceBetween: 24
+        // keyboard: {
+        //     enabled: true,
+        //     onlyInViewport: false,
+        // },
+        autoplay: {
+            delay: 3000,
         },
-        // when window width is >= 960px
-        960: {
-            slidesPerView: 3,
-            spaceBetween: 24
+        breakpoints: {
+            // when window width is >= 600px
+            600: {
+                slidesPerView: 1.4,
+                spaceBetween: 24
+            },
+            // when window width is >= 768px
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 24
+            },
+            // when window width is >= 960px
+            960: {
+                slidesPerView: 3,
+                spaceBetween: 24
+            }
         }
+    });
+
+    // 영화차트 탭 메뉴
+    var movBtn = $(".movie_title > ul > li");
+    var movCont = $(".movie_chart > div");
+        // 버튼도 4개 컨텐츠도 4개.
+
+    movCont.hide().eq(0).show();
+    /*
+        .eq() : 순서대로 정한다.
+        첫번째의 컨텐츠만 나오도록 설정함. 
+    */
+
+    movBtn.click(function(e){
+        e.preventDefault();
+        var target = $(this);
+        /*  
+            this는 자기 자신이라는 뜻, 버튼 클릭한 곳으로 가게 된다.
+            사용자가 몇번째 버튼을 클릭했는지 확인할 수 있다. 
+        */
+        var index = target.index();
+        /*
+            사용자가 몇번째 버튼을 클릭했는지 우리는 눈으로 볼 수 가 없으니까,
+            index라는 메서드를 이용해서 이 target한테 번호를 매겨준다. 
+        */
+        movBtn.removeClass("active");
+        // js코드를 짤때는 제거를 먼저시켜준다음에 실행하도록 해야 한다. 
+        target.addClass("active");
+        movCont.css("display","none");
+        // 안보이게 하기위해서 .hide를 써도 되지만, css를 써도 상관없다.
+        movCont.eq(index).css("display","block");
+        // movCont(컨텐츠)의 번호(변수 index -> target.index())의 해당되는 것만 보여주게 만들어 준 것.
+    });
+
+    // 공지사항 탭 메뉴
+    var tapMenu = $(".notice");
+
+    // 컨텐츠 내용을 숨겨주세요!
+    tapMenu.find("ul > li > ul").hide();
+    // 먼저 찾아야 한다. ul 안에 있는 ul을 찾은 다음, 숨긴다.
+    tapMenu.find("li.active > ul").show();
+    // 여기서 li에 .active가 붙은거의 자식 ul만 보여지게 작업을 한다. 
+
+    function tabList(e){
+        e.preventDefault();
+        // a태그의 href="#"의 기능을 차단 시켜준다. 
+        var target = $(this);
+        // 누가 정해질지 모르니가 변수로 target을 만든다.
+        target.next().show().parent("li").addClass("active").siblings("li").removeClass("active").find("ul").hide();
+        /*
+            여기서 next는 a태그의 형제인 ul (next() => 옆태그(형제태그)). ul태그를 show(보여주고) ul태그의 parent, 즉 부모 태그를 찾을 것이다. parent("li") => li라는 부모를 찾는다. 
+            addClass로 .active를 붙여주고, siblings("li")=> 그 형제인 li들을 찾아서, removeClass로 .active를 지워준다.
+            find로 ul을 찾아서 hide로 지워준다. 
+        */
+
+        /*
+            1. 버튼(a태그)을 클릭하면 부모관계의 li에게 .active를 추가시켜주고, 부모의 형제인 다른 li한테 있는 .active는 삭제시켜준다. 
+            2. 그리고 a태그의 형제관계인 ul을 보여줄 것이다. .active가 삭제된 li의 자식태그인 ul은 안보이게 해준다.
+        */
+
+        // 버튼을 클릭하면 형제의 ul을 우선 보여주고, 
+        // 부모의 li태그에 클래스를 추가하고,
+        // 선택된 li를 제외한 다른 형제들(li)의 클래스를 제거하고, 
+        // 제거한 자식의 ul 태그를 숨겨준다. 
     }
-});
 
-// 영화차트 탭 메뉴
-var movBtn = $(".movie_title > ul > li");
-var movCont = $(".movie_chart > div");
-    // 버튼도 4개 컨텐츠도 4개.
+    tapMenu.find("ul > li > a").click(tabList).focus(tabList);
+    // tabMenu( = .notice)에 find를 이용해서 ul > li > a태그를 클릭했을 때, tabList를 실행한다.  
+    // tab키를 눌렀을때 전체공지탭에서 아래 내용을 읽어야 하기때문에, focus(tabList)를 준다. 
 
-movCont.hide().eq(0).show();
-/*
-    .eq() : 순서대로 정한다.
-    첫번째의 컨텐츠만 나오도록 설정함. 
-*/
-
-movBtn.click(function(e){
-    e.preventDefault();
-    var target = $(this);
-    /*  
-        this는 자기 자신이라는 뜻, 버튼 클릭한 곳으로 가게 된다.
-        사용자가 몇번째 버튼을 클릭했는지 확인할 수 있다. 
-    */
-    var index = target.index();
-    /*
-        사용자가 몇번째 버튼을 클릭했는지 우리는 눈으로 볼 수 가 없으니까,
-        index라는 메서드를 이용해서 이 target한테 번호를 매겨준다. 
-    */
-    movBtn.removeClass("active");
-    // js코드를 짤때는 제거를 먼저시켜준다음에 실행하도록 해야 한다. 
-    target.addClass("active");
-    movCont.css("display","none");
-    // 안보이게 하기위해서 .hide를 써도 되지만, css를 써도 상관없다.
-    movCont.eq(index).css("display","block");
-    // movCont(컨텐츠)의 번호(변수 index -> target.index())의 해당되는 것만 보여주게 만들어 준 것.
-});
-
-// 공지사항 탭 메뉴
-var tapMenu = $(".notice");
-
-// 컨텐츠 내용을 숨겨주세요!
-tapMenu.find("ul > li > ul").hide();
-// 먼저 찾아야 한다. ul 안에 있는 ul을 찾은 다음, 숨긴다.
-tapMenu.find("li.active > ul").show();
-// 여기서 li에 .active가 붙은거의 자식 ul만 보여지게 작업을 한다. 
-
-function tabList(e){
-    e.preventDefault();
-    // a태그의 href="#"의 기능을 차단 시켜준다. 
-    var target = $(this);
-    // 누가 정해질지 모르니가 변수로 target을 만든다.
-    target.next().show().parent("li").addClass("active").siblings("li").removeClass("active").find("ul").hide();
-    /*
-        여기서 next는 a태그의 형제인 ul (next() => 옆태그(형제태그)). ul태그를 show(보여주고) ul태그의 parent, 즉 부모 태그를 찾을 것이다. parent("li") => li라는 부모를 찾는다. 
-        addClass로 .active를 붙여주고, siblings("li")=> 그 형제인 li들을 찾아서, removeClass로 .active를 지워준다.
-        find로 ul을 찾아서 hide로 지워준다. 
-    */
-
-    /*
-        1. 버튼(a태그)을 클릭하면 부모관계의 li에게 .active를 추가시켜주고, 부모의 형제인 다른 li한테 있는 .active는 삭제시켜준다. 
-        2. 그리고 a태그의 형제관계인 ul을 보여줄 것이다. .active가 삭제된 li의 자식태그인 ul은 안보이게 해준다.
-    */
-
-    // 버튼을 클릭하면 형제의 ul을 우선 보여주고, 
-    // 부모의 li태그에 클래스를 추가하고,
-    // 선택된 li를 제외한 다른 형제들(li)의 클래스를 제거하고, 
-    // 제거한 자식의 ul 태그를 숨겨준다. 
-}
-
-tapMenu.find("ul > li > a").click(tabList).focus(tabList);
-// tabMenu( = .notice)에 find를 이용해서 ul > li > a태그를 클릭했을 때, tabList를 실행한다.  
-// tab키를 눌렀을때 전체공지탭에서 아래 내용을 읽어야 하기때문에, focus(tabList)를 준다. 
+})(jQuery);
--- js/movie.js
+++ js/movie.js
@@ -1,95 +1,117 @@
+'use strict';
 (function($){
 
     /* 트레일러 영상 플레이어를 활성화 */
-    /* YouTube iframe API: https://developers.google.com/youtube/player_parameters */
-    
-    (function handleTrailer() {
-        // 셀렉터 캐시
-        var $selector = {
-            body: $( "body" ),
-            overlay: $( "#blackout" ),
-            modal: $( "#trailerModal" ),
-            showButton: $( "#showTrailer" ),
-            hideButton: $( "#hideTrailer" ),
+    /* youtube iframe API : https://developers.google.com/youtube/player_parameters */
+
+    (function handleTrailer(){
+        // 셀렉터 캐시 => 선택자를 미리 만들어 놓기 위해.
+        /* 
+            var selector = $(".selector"); => 선택자 만드는 기본적인 방법.
+            기본적인 변수 선언방법
+            var body = $("body"),
+                overlay = $("blackout"),
+                modal = $("#trailerModal"),
+                showButton = $("#showTrailer"),
+                hideButton = $("#hideTrailer"),
+        */
+
+        // 객체를 통한 변수 선언방법
+        var $selector = {           // [] => 배열을 만들 때, {} => 객체를 만들 때
+            body: $("body"),
+            overlay: $("#blackout"),
+            modal: $("#trailerModal"),
+            showButton: $("#showTrailer"),
+            hideButton: $("#hideTrailer"),
+            // 여러가지 변수를 객체를 통해서 한꺼번에 만들 수 있다. 
         };
-        
-        // 플레이어
+
+        // 플레이어 API 설정
         var player = {
-            obj: null, // 플레이어 오브젝트
-            query : {
+            obj: null,  // 플레이어 오브젝트 -> null값 자리에 넣어서 컨트롤 하기 위함.
+            query : {   // query는 매개변수 부분.
                 theme: "dark",
                 color: "white",
-                controls: 1, 
-                autoplay: 1,
-                enablejsapi: 1,
-                modestbranding: 0, // YouTube 로고 감춤
-                rel: 0,  // 관련 동영상 표시
+                controls: 1,
+                autoplay: 1, // 플레이어가 로드 될 때 초기 동영상을 자동재생
+                enablejsapi: 1, // JavaScriptAPI를 사용하도록 설정된다. 
+                modestbranding: 0, // youtube 로고 감춤
+                rel: 0, // 제목, 업로더 감춤
                 showinfo: 0, // 제목, 업로더 감춤
                 iv_load_policy: 3 // 특수효과 감춤
             },
-            visible: false
+            visible: false // false는 안보이게 한다. 
         };
 
-        // 보이기, 숨기기 버튼 활성화
-        $selector.showButton.on( "click", showPlayer );
-        $selector.hideButton.on( "click", hidePlayer );
+        // 보이기 버튼, 숨기기 버튼 활성화
+        $selector.showButton.on("click",showPlayer);
+        $selector.hideButton.on("click",hidePlayer);
 
-        // YouTube API를 이용해 iframe을 생성
-        function setPlayer( id ) {
-            player.obj = new YT.Player( "trailer", {
+        // youtube API를 이용해서 iframe을 생성
+        /* 기존 youtube에서 공유-> 동영상 퍼가기의 iframe을 사용하면 단점은 기본 youtube틀이 다 나오고 API를 디테일하게 컨트롤 할 수 없고, 코드를 다 넣어줘야 한다. */
+        // 스크립트 코드로 만드는 법.
+        function setPlayer(id){ 
+            // setPlayer에서 가져온 data("youtube")값인  "F1239ZePXfM"를 id에 넣어준다. 
+            /* 
+            id라고 적은 것은 youtube영상 id => videoID => 영상마다 동영상 퍼가기의 iframe을 보면 id가 있다. https://www.youtube.com/embed/Vj8WvqM7kXs => Vj8WvqM7kXs 이게 ID 
+            */
+            player.obj = new YT.Player("trailer", { 
+                // 아무것도 없는 obj:null 값에 새로운 trailer를 넣어 준것.
                 width: 480,
                 height: 282,
-                videoId: id,    
+                videoId: id, // id라는 매개변수를 넣어준 것
                 playerVars: player.query
             });
 
-            // 처음 플레이어 크기 설정
+            // 처음 플레이어 크기 설정 
             resizePlayer();
 
-            // 리사이즈나 화면 회전시 플레이어 크기 다시 설정
-            $( window ).on( "resize orientationchange", function() {
+            // 리사이즈 화면 회전시 플레이어 크기 다시 설정 
+            $(window).on("resize orientationchange", function() {
                 resizePlayer();
             });
+            
         }
 
-        // 화면 크기에 비례해 iframe의 크기 조절
-        function resizePlayer() {
+        function resizePlayer(){ // (화면크기를 자동으로 변경해주는 것 설정)
             var viewport = {}, frame = {}, modal = {};
 
-            viewport.width = $( window ).width();
-            viewport.height = $( window ).height();
+            viewport.width = $(window).width(); // 화면 크기를 현재 widow의 크기로 구하기
+            viewport.height = $(window).height(); // 화면 높이를 현재 widow의 크기로 구하기
 
-            frame.width = viewport.width;
-            frame.height = frame.width / 1.6; // 16 : 10
+            frame.width = viewport.width; // 프레임 크기를 화면의 크기로 구하기
+            frame.height = frame.width / 1.6; // 프레임 높이 값은 화면 크기의 비율로 16 : 10 으로 조정.
 
-            modal.top = ( ( viewport.height - frame.height ) / 2 ) + "px";
-            modal.left = "0px";
+            // modal을 띄워야 하니까 띄울려면 top값과 left값이 필요 -> position을 설정해 놨기 때문.
+            modal.top = ((viewport.height - frame.height) / 2)+"px";  // (전체)화면 높이에서 (iframe의)프레임의 높이를 빼주고 나누기 2해주고(중앙으로 오게 하기 위해서) px단위 붙여주기. 
+            modal.left = "0px"; // 어차피 좌우로는 붙어있어야 하니까 움직일 필요가 없으니까 0px
 
-            $selector.modal.css( modal );
-
-            player.obj.setSize( frame.width, frame.height );
+            $selector.modal.css(modal); // css로 .modal을 만들어 놓고, position: absolute만 준 이유는 script로 top, left값을 주기 위함이다. 
+            
+            player.obj.setSize(frame.width, frame.height); // obj가 null값이었던 것을 값을 넣어준 것.
         }
 
         // iframe 보이기
-        function showPlayer() {
-            // 처음 클릭시 iframe을 생성 
-            if ( !player.obj ) {
-                setPlayer( $selector.showButton.data("youtube") );
+        function showPlayer(){ // 시작버튼을 누르면 이게 활성화
+            if (!player.obj) { // obj가 null값이 아니면,
+                setPlayer($selector.showButton.data("youtube")); // data-youtube="F1239ZePXfM" html코드에 적어놓은 data를 showButton을 누르면 가져온다. 
             }
 
             $selector.body.addClass("modal_on");
-            $selector.overlay.show();
-            player.visible = true;
-        }
+            $selector.overlay.show(); // $selector.overlay는 위에 변수로 설정을 해놨다. 
+            player.visible = true; // visible을 true 작업을 해주니까 보인다. 
+        };
 
-        // iframe 감추기
-        function hidePlayer() {
+        //  iframe 감추기 
+        function hidePlayer(){ // 닫기버튼을 누르면 이게 활성화
             player.obj.stopVideo();
             $selector.overlay.hide();
             $selector.body.removeClass("modal_on");
             player.visible = false;
-        }
+        };
 
     })();
 
-})(jQuery);     
+})(jQuery);
+
+// (function($){})(jQuery); -> 시작하자마자 jquery를 읽을 수 있도록 설정해준 것.
